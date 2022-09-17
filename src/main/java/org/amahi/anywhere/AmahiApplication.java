e748f38781f17c150a4dfb5fc60cbcc21dfb1f75
whsgzcy <whsgzcy@foxmail.com>
2022-09-17 13:34:21 +0800

    chongtu3
    Change-Id: I2b28be77fcce9498b464041ff4bac4dcf396b420

--- src/main/java/org/amahi/anywhere/AmahiApplication.java
+++ src/main/java/org/amahi/anywhere/AmahiApplication.java
@@ -156,27 +156,22 @@ public class AmahiApplication extends Application {
         int importanceDownload = NotificationManager.IMPORTANCE_LOW;
         int importanceUpload = NotificationManager.IMPORTANCE_LOW;
 
-        android.util.Log.i("whsgzcy", "123");
-        NotificationChannel downloadChannell = new NotificationChannel(DOWNLOAD_CHANNEL_ID, getString(R.string.download_channel), importanceDownload);
-         // ifadd
-         // super.yu@GerritTest,2022/09/17 13:11:11,this is a test
-         for(int i = 0; i < 10; i++) {
-             System.out.println("this is a test");
-         }
-         // endif
-        downloadChannell.setDescription(getString(R.string.download_channel_desc));
-        android.util.Log.i("whsgzcy", "123");
+        NotificationChannel uploadChannel = new NotificationChannel(UPLOAD_CHANNEL_ID, getString(R.string.upload_channel), importanceUpload);
+    `   // ifadd
+        // super.yu@GerritTest,2022/09/17 13:11:11,this is a test
+        for(int i = 0; i < 10; i++) {
+            System.out.println("this is a test");
+        }
+        // endif
+        uploadChannel.setDescription(getString(R.string.upload_channel_desc));
 
-        android.util.Log.i("whsgzcy", "456");
+        NotificationChannel downloadChannel = new NotificationChannel(DOWNLOAD_CHANNEL_ID, getString(R.string.download_channel), importanceDownload);
+        downloadChannel.setDescription(getString(R.string.download_channel_desc));
+
         // Once the channel is registered, it's importance and behaviour can't be changed
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
         notificationManager.createNotificationChannel(uploadChannel);
         notificationManager.createNotificationChannel(downloadChannel);
-        android.util.Log.i("whsgzcy", "456");
-
-        NotificationChannel uploadChannelTest = new NotificationChannel(UPLOAD_CHANNEL_ID, getString(R.string.upload_channel), importanceUpload);
-        uploadChannelTest.setDescription(getString(R.string.upload_channel_desc));
-
         
     }
 }
